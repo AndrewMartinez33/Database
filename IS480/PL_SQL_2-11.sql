415b5304fc2134ae763cf3b39a24eca2caf67a3f
Andrew A. Martinez <andrew.a.martinez33@gmail.com>
2019-04-14 13:39:27 -0700

    hw assignment

---
+++ IS480/PL_SQL_2-11.sql
@@ -0,0 +1,16 @@
+SET SERVEROUTPUT ON
+CREATE or REPLACE PROCEDURE MyReplaceOne (p_text varchar2, p_char1 varchar2, p_char2 varchar2) AS 
+
+	v_before varchar2(200);
+	v_after varchar2(200);
+
+BEGIN
+	v_before := substr(p_text, 1, instr(p_text, p_char1) - 1);
+	v_after := substr(p_text, instr(p_text, p_char1) + length(p_char1));
+	dbms_output.put_line(v_before||p_char2||v_after);
+
+END;
+/
+
+
+exec MyReplaceOne('1234-**5678', '-**', '#&&');
